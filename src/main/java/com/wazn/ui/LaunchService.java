b5f27eaee794e0f35676e89281c97f0c0f6baac6
Mohammed Kherfan <mohammed.kherfan@progressoft.com>
2018-11-18 22:14:32 +0200

    First Commit

---
+++ src/main/java/com/wazn/ui/LaunchService.java
@@ -0,0 +1,12 @@
+package com.wazn.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchService {
+
+    public static void main(String [] args) {
+        SpringApplication.run(LaunchService.class, args);
+    }
+}
